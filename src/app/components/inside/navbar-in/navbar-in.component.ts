e345d74b871656730c8bff640a6553a961815786
IsaacMizrahi123 <eipalacio@uninorte.edu.co>
2020-06-09 19:10:15 -0500

    Aporte de Isaac

--- src/app/components/inside/navbar-in/navbar-in.component.ts
+++ src/app/components/inside/navbar-in/navbar-in.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-navbar-in',
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar-in.component.css']
 })
 export class NavbarInComponent implements OnInit {
+
+	@Output() SignOutEmitter = new EventEmitter<any>();
+
+	SignOut(){
+		this.SignOutEmitter.emit();
+	}
 
   constructor() { }
 
